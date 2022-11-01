1e8ab432ba8b7faf2f882f1c5663f12fa5a0a131
Abdullah Saikat <abdullahsaikat77@gmail.com>
2022-11-01 19:17:14 +0600

    add Table

--- asset_manage/models.py
+++ asset_manage/models.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+from email.policy import default
 from django.db import models
+from django.utils.timezone import now
 
 
 class Asset(models.Model):
     title = models.CharField(max_length=20)
+    owner = models.CharField(max_length=20,default='')
+    date = models.DateField(default=datetime.now)
     description = models.CharField(max_length=100)
     price = models.IntegerField()
+
